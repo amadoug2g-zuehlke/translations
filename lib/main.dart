5e920da63f2f16ee2fcfa1c0548fba436b21b087
Amadougaye Cissé <amadougaye.cisse@zuehlke.com>
2023-07-28 09:58:31 +0200

    Implementing custom language translations Adding json translations to pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Localization',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Localization Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({required this.title, super.key});
 
   final String title;
 
